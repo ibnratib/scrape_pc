a223d04a45a522103c1091a4d461a176df5d4c72
ibnratib <youssef.ibnou-ratib@uit.ac.ma>
2023-04-29 13:25:38 +0200

    merge code

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, ajax_calls, download_file
+from .views import home, ajax_calls
 
 urlpatterns = [
     path('', home, name='home'),
     path("ajax-calls/", ajax_calls, name='ajax_calls'),
-    path('download/', download_file, name='download_file'),
 
 
 ]
